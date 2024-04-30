853c747737a7ce633c1617969baa73ec48181ce2
eunjin917 <cgoya01@likelion.org>
2024-04-30 10:57:48 +0900

    init: backend 초기 설정 #1

---
+++ backend/src/main/java/com/hanaro/hanaria/HanariaApplication.java
@@ -0,0 +1,13 @@
+package com.hanaro.hanaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanariaApplication.class, args);
+	}
+
+}
