2754e4c6fa8359b2cdf495c430c33f8f848623c7
eunjin917 <cgoya01@likelion.org>
2024-05-03 17:49:56 +0900

    feat: coupon(R) 관리자페이지 #6

--- backend/src/main/java/com/hanaro/hanaria/domain/coupon/CouponController.java
+++ backend/src/main/java/com/hanaro/hanaria/domain/coupon/CouponController.java
@@ -1,6 +1,8 @@
 package com.hanaro.hanaria.domain.coupon;
 
+import com.hanaro.hanaria.domain.product.ProductService;
 import com.hanaro.hanaria.dto.coupon.CouponFindAllResponseDto;
+import com.hanaro.hanaria.dto.product.ProductFindAllResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CouponController {
     private final CouponService couponService;
+    private final ProductService productService;
 
     @GetMapping("/coupons")
     public String adminCoupons(Model model
     ) {
         List<CouponFindAllResponseDto> couponFindAllResponseDtoList = couponService.findAll();
+        List<ProductFindAllResponseDto> productFindAllResponseDtoList = productService.findAll();
         model.addAttribute("list", couponFindAllResponseDtoList);
+        model.addAttribute("products", productFindAllResponseDtoList);
         return "/pages/coupon/coupons";
     }
 }
