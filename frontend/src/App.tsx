f6cf839b819cd1fd0f8f01b243e9d7e5cc06076b
wldnd9904 <wldnd9904@uos.ac.kr>
2024-05-08 23:05:14 +0900

    feat: 옵션 선택, 상품 담기 완료 #20

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,66 +1,7 @@
-import { useState } from "react";
-import PinkBox from "./components/common/PinkBox";
-import { HStack, Spacer, VStack } from "./components/common/Stack";
+import Products from "./routes/Products";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="w-full h-full">
-      <h1>하나리아</h1>
-      <div className="card">
-        <button
-          className="border border-red-500 "
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <HStack>
-          <VStack>
-            <PinkBox />
-            <PinkBox />
-            <PinkBox />
-            <PinkBox />
-          </VStack>
-
-          <VStack>
-            <PinkBox />
-            <PinkBox />
-            <Spacer />
-            <PinkBox />
-          </VStack>
-
-          <VStack>
-            <Spacer />
-            <PinkBox />
-            <PinkBox />
-            <PinkBox />
-          </VStack>
-
-          <VStack>
-            <PinkBox />
-            <PinkBox />
-            <PinkBox />
-            <Spacer />
-          </VStack>
-
-          <VStack>
-            <Spacer />
-            <PinkBox />
-          </VStack>
-        </HStack>
-        <div className="flex flex-col items-center w-48 h-48 rounded-xl shadowed font-bold leading-none">
-          <img src="https://img.lotteeatz.com/upload/product/2023/02/10/20230210102118259_1.png" />
-          <span className="mb-2">한우불고기</span>
-          <HStack>
-            <button className="rounded shadowed w-4 h-4">-</button>
-            <span className="w-4 text-center">1</span>
-            <button className="rounded shadowed w-4 h-4">+</button>
-          </HStack>
-        </div>
-      </div>
-    </div>
-  );
+  return <Products />;
 }
 
 export default App;
--- frontend/src/components/product/ItemSelector.tsx
+++ frontend/src/components/product/ItemSelector.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import ItemType from "../../types/ItemType";
 import ProductType from "../../types/ProductType";
 import GroupType from "../../types/GroupType";
 import { HStack, VStack } from "../common/Stack";
@@ -16,11 +15,14 @@ import {
 import OptionCell from "./OptionCell";
 import OptionType from "../../types/OptionType";
 import Button from "../common/Button";
+import RequestSelecor from "./RequestSelector";
+import Title from "../common/Title";
+import LocalItemType from "../../types/local/LocalItemType";
 
 interface ItemSelectorProps {
   group: GroupType;
   onClose: () => void;
-  onSelect: (item: ItemType) => void;
+  onSelect: (localItem: LocalItemType) => void;
 }
 
 function ItemSelector({ group, onClose, onSelect }: ItemSelectorProps) {
@@ -29,12 +31,16 @@ function ItemSelector({ group, onClose, onSelect }: ItemSelectorProps) {
   const [dessertOption, setDessertOption] = useState<OptionType | null>(null);
   const [drinkOption, setDrinkOption] = useState<OptionType | null>(null);
   const [allSelected, setAllSelected] = useState<boolean>(false);
+  const [requests, setRequests] = useState<number>(0);
 
+  //총 가격 계산
   const totalPrice =
     ((product?.price ?? 0) +
       (dessertOption?.price ?? 0) +
       (drinkOption?.price ?? 0)) *
     count;
+
+  //상품 수량 업다운
   const countUp = () => setCount((count) => count + 1);
   const countDown = () => {
     if (count > 1)
@@ -43,20 +49,48 @@ function ItemSelector({ group, onClose, onSelect }: ItemSelectorProps) {
       });
   };
   useEffect(() => {
+    if (product == null) return;
     if (demoDessertOptions.length > 0) setDessertOption(demoDessertOptions[0]);
     if (demoDrinkOptions.length > 0) setDrinkOption(demoDrinkOptions[0]);
-  }, [demoDessertOptions, demoDrinkOptions]);
+  }, [product, demoDessertOptions, demoDrinkOptions]);
+
+  //선택한 상품 초기화
+  const clearProduct = () => {
+    setProduct(null);
+    setCount(1);
+    setDessertOption(null);
+    setDrinkOption(null);
+    setRequests(0);
+  };
+
+  // 주문담기
+  const confirm = () => {
+    const localItemDraft: LocalItemType = {
+      price: totalPrice,
+      count: count,
+      request: requests,
+      product: product,
+      dessertOption: dessertOption,
+      drinkOption: drinkOption,
+    };
+    onSelect(localItemDraft);
+    onClose();
+  };
 
   // 1: 상품 선택
   if (product == null)
     return (
-      <ProductSelector products={group.productList} onSelect={setProduct} />
+      <VStack>
+        <Title>상품 선택</Title>
+        <ProductSelector products={group.productList} onSelect={setProduct} />
+      </VStack>
     );
 
   // 2: 상품 수량, 옵션 선택
   if (!allSelected)
     return (
       <VStack>
+        <Title>상품 수량 및 옵션 선택</Title>
         <HStack className="items-center">
           <img
             className="bg-white w-36 h-36 object-contain rounded-xl shadowed mb-2"
@@ -131,9 +165,9 @@ function ItemSelector({ group, onClose, onSelect }: ItemSelectorProps) {
         <HStack className="justify-center mt-2">
           <Button
             className="bg-red-400 font-bold text-white mr-2"
-            onClick={onClose}
+            onClick={clearProduct}
           >
-            취소하기
+            뒤로가기
           </Button>
           <Button
             className="bg-blue-400 font-bold text-white"
@@ -141,14 +175,32 @@ function ItemSelector({ group, onClose, onSelect }: ItemSelectorProps) {
               setAllSelected(true);
             }}
           >
-            주문담기
+            선택완료
           </Button>
         </HStack>
       </VStack>
     );
 
   // 3: 주문 담기 직전 요청사항
-  return <></>;
+  return (
+    <VStack>
+      <Title>요청사항 선택</Title>
+      <RequestSelecor requests={requests} onSelect={setRequests} />
+      <HStack className="justify-center mt-4">
+        <Button
+          className="bg-red-400 font-bold text-white mr-2"
+          onClick={() => {
+            setAllSelected(false);
+          }}
+        >
+          뒤로가기
+        </Button>
+        <Button className="bg-blue-400 font-bold text-white" onClick={confirm}>
+          주문담기
+        </Button>
+      </HStack>
+    </VStack>
+  );
 }
 
 export default ItemSelector;
